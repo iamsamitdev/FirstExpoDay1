12b48d6dde7442db5767cc09e9b200f9b89a9d1c
Samit Koyom <samitkoyom@gmail.com>
2024-11-19 00:29:22 +0700

    initial commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.content}>Hello React Native by Expo</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'salmon',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  content: {
+    width: '80%',
+    color: '#fff',
+    fontSize: 36,
+    textAlign: 'center',
+  }
 });
